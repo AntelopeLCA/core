079f6d1195dc4644366d0aa74319f65758f629e8
Brandon Kuczenski <brandon.kuczenski@301south.net>
2021-06-08 14:32:22 -0700

    Try configuration changes to avoid circular deps on CI

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.1.6-2'
+VERSION = '0.1.7'
 
 requires = [
     "synonym_dict>=0.2.0",
@@ -13,6 +13,7 @@ requires = [
 # optional: pylzma
 """
 Version History
+0.1.7 - 2021-06-08 - [virtualize] in-progress
 0.1.6 - 2021-03-10 - update to handle new synonym_dict 0.2.0, along with OLCA reference flow matching, ecoinvent 2.2,
                      a range of other improvements in performance and context handling
                      2021-03-10 post1
