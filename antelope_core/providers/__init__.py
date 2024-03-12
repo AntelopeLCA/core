de6501160ff7734b92b86cfb27a6734b53adea64
Brandon Kuczenski <brandon.kuczenski@301south.net>
2024-03-12 14:27:48 -0700

    Update OLCA to include v2 schema. also ConversionError.

--- antelope_core/providers/__init__.py
+++ antelope_core/providers/__init__.py
@@ -18,7 +18,7 @@ except ImportError:
     _prov = []
 
 from .ecoinvent_lcia import EcoinventLcia
-from .openlca_jsonld import OpenLcaJsonLdArchive
+from .openlca import OpenLcaJsonLdArchive
 from .traci import Traci21Factors
 from .xdb_client import XdbClient
 
