55a5e07f1c83e562ffebd794c1d6c3e2d5c8c70f
Brandon Kuczenski <brandon.kuczenski@301south.net>
2024-10-09 02:24:58 -0700

    pull out n2 and organic n

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.4.1'
+VERSION = '0.3.4.3'
 
 requires = [
     "synonym_dict>=0.2.4",
@@ -14,6 +14,7 @@ requires = [
 # optional: pylzma
 """
 Version History
+0.3.4.3 2024-10-09 - fixed a flowables issue: gaseous Nitrogen | organic nitrogen. also removed a big data file.
 0.3.4.1 2024-10-09 - fixed a flowables issue: PM2.5 and PM10 had become merged.
 
 0.3.4  2024-09-22  - workshop release.  Ditch make_tester()
