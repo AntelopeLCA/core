c327f5137428834cecba205538d2b25fe3b12c33
Brandon Kuczenski <brandon.kuczenski@301south.net>
2025-09-08 12:01:00 -0700

    Release 0.3.7

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.6'
+VERSION = '0.3.7'
 
 requires = [
     "synonym_dict>=0.2.4",
-    "antelope_interface>=0.3.2",
+    "antelope_interface>=0.3.3",
     "xlstools>=0.1.6",
     "python-magic>=0.4.18",
     "requests>=2.25",
@@ -14,6 +14,7 @@ requires = [
 # optional: pylzma
 """
 Version History
+0.3.7 2025-09-08  - LCIA results output to pandas
 0.3.6 2025-08-15  - bump bg interface changes
 0.3.5 2025-06-18  - bug fix catch-up release for ISSST workshop
 0.3.4.4 2024-10-24 - fetch remote contexts when reading in remote LCIA results. 
